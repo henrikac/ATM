0e84a262d07441b21764e28ab03fd68682a20595
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-10-17 20:04:35 +0200

    updated comment

--- src/ATM.c
+++ src/ATM.c
@@ -2,7 +2,17 @@
  * Programmer: Henrik A. Christensen     Date Completed: in progress
  * Instructor: Kurt Nørmark              Class:          Imperative Programming
  *
- * Coming soon...
+ * American ATM
+ * available banknotes | $100, $50, $20 and $10
+ * 
+ * User enters how much he/she wants to withdraw
+ * and the ATM should return the fewest number of banknotes
+ * 
+ * examples:
+ * 
+ * 180 should return 1x $100, 1x $50, 1x $20 and 1x $10
+ *  10 should return 0x $100, 0x $50, 0x $20 and 1x $10
+ * 570 should return 5x $100, 1x $50, 1x $20 and 0x $10
 */
 
 #include <stdio.h>
